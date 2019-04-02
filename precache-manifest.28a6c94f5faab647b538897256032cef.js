9d8eb16f24fdd475edef211ddb8053753df3890e
Valentyn <valantine10010@gmail.com>
2019-04-02 19:00:44 +0300

    Updates

---
+++ precache-manifest.28a6c94f5faab647b538897256032cef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "17ff21546a86fb59c33de9986bcd0647",
+    "url": "/portfolio/static/media/img-mobile-2.17ff2154.jpg"
+  },
+  {
+    "revision": "05cb2d18abc6307b03a2",
+    "url": "/portfolio/static/css/main.7c23aa8b.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "84ecee60d605428098ce010a87135c97",
+    "url": "/portfolio/static/media/sprite.84ecee60.png"
+  },
+  {
+    "revision": "c65b4a169e5fd011a076134de4a8ca74",
+    "url": "/portfolio/static/media/cd-icon-arrow.c65b4a16.svg"
+  },
+  {
+    "revision": "2de26e169c196a5560b79627de70bf20",
+    "url": "/portfolio/static/media/img-mobile-1.2de26e16.jpg"
+  },
+  {
+    "revision": "05cb2d18abc6307b03a2",
+    "url": "/portfolio/static/js/main.59bd1463.chunk.js"
+  },
+  {
+    "revision": "d0b0eabf6e6be2996ec0b2fbfd8cce72",
+    "url": "/portfolio/static/media/img-mobile-3.d0b0eabf.jpg"
+  },
+  {
+    "revision": "d8eabfce199cc68979305be898dd3749",
+    "url": "/portfolio/static/media/img-mobile-4.d8eabfce.jpg"
+  },
+  {
+    "revision": "23631fe82385f66524c0d4a303c26ac2",
+    "url": "/portfolio/static/media/img-2.23631fe8.jpg"
+  },
+  {
+    "revision": "6be66374abfea1e6492fea4b5f46c959",
+    "url": "/portfolio/static/media/img-1.6be66374.jpg"
+  },
+  {
+    "revision": "66a2a21b490f0287ec60db22d54f2c43",
+    "url": "/portfolio/static/media/img-3.66a2a21b.jpg"
+  },
+  {
+    "revision": "ea7cb0c1e6c8970aaf448be752ff8449",
+    "url": "/portfolio/static/media/img-4.ea7cb0c1.jpg"
+  },
+  {
+    "revision": "fb2c00704a536ce49826fbbd5c6f2e04",
+    "url": "/portfolio/index.html"
+  }
+];
